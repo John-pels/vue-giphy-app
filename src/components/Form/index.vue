6eebbfa139f1933da62f94fc7e1a929be23f62d9
Ajeigbe John Oluwaseyi <ajeigbejohnolu@gmail.com>
2021-01-09 01:17:26 +0100

    I create form component

---
+++ src/components/Form/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <form class="form">
+      <div class="input-group">
+        <input type="text" placeholder="Enter a keyword" class="input" />
+      </div>
+      <button class="btn-search">Search</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Form",
+};
+</script>
+
+<style scoped>
+@import "./style.css";
+</style>
