1dbb92dd37e1a93c9974c93a4297697078315060
Ajeigbe John Oluwaseyi <ajeigbejohnolu@gmail.com>
2021-01-09 01:15:32 +0100

    I create ListItems component

---
+++ src/components/ListItems/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="list-item">
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </section>
+</template>
+
+<script>
+import Card from "@/components/Card";
+
+export default {
+  name: "ListItems",
+  components: {
+    Card,
+  },
+};
+</script>
+
+<style scoped>
+.list-item {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 1rem;
+}
+</style>
