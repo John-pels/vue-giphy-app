7b89823e8c8b48b142d5be2f151f6d00dc66c0e8
Ajeigbe John Oluwaseyi <ajeigbejohnolu@gmail.com>
2021-01-09 01:15:54 +0100

    I create container component

---
+++ src/components/container.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Container",
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 80%;
+  margin: 0 auto;
+}
+
+@media (max-width: 768px) {
+  .container {
+    max-width: 100%;
+  }
+}
+</style>
