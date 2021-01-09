85f84afa83be47306526ce4032759c59cb95adfa
Ajeigbe John Oluwaseyi <ajeigbejohnolu@gmail.com>
2021-01-09 01:18:38 +0100

    I update gif details oage

--- src/views/Gif_details.vue
+++ src/views/Gif_details.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is a Gif details page</h1>
-  </div>
+  <h1 class="h1">Gif Details</h1>
+  <Container :class="flex">
+    <GifDetails />
+  </Container>
 </template>
+
+<script>
+import GifDetails from "@/components/GifDetails";
+import Container from "@/components/container";
+export default {
+  components: {
+    GifDetails,
+    Container,
+  },
+  data() {
+    return {
+      flex: "flex",
+    };
+  },
+};
+</script>
+<style scoped>
+.h1 {
+  text-align: center;
+  padding: 1.5rem 0;
+}
+.flex {
+  display: flex;
+  justify-content: center;
+}
+
+@media (max-width: 768px) {
+  .flex {
+    padding: 0 1rem;
+  }
+}
+</style>
